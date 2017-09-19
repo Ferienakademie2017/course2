5722633681e4625f7e6a292037e419b34e367221
Mathias Jackermeier <mathias.jackermeier@tum.de>
2017-09-19 17:43:03 +0200

    Implemented a direct linear mapping using Tensorflow

---
+++ main.py
@@ -0,0 +1,64 @@
+import tensorflow as tf
+import scipy.misc as misc
+import numpy as np
+
+def weight_variable(shape):
+    initial = tf.truncated_normal(shape)
+    return tf.Variable(initial)
+
+def bias_variable(shape):
+    initial = tf.truncated_normal(shape)
+    return tf.Variable(initial)
+
+
+x = tf.placeholder(tf.float32, [1, 1])
+
+#output layer
+w_fc1 = weight_variable([1, 4096])
+b_fc1 = bias_variable([4096])
+output = tf.matmul(x, w_fc1) + b_fc1
+
+ground_truth = tf.placeholder(tf.float32, [1, 4096])
+loss = tf.reduce_mean(tf.reduce_sum(tf.pow(ground_truth - output, 2), reduction_indices=[1]))
+train_step = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+
+sess = tf.InteractiveSession()
+tf.global_variables_initializer().run()
+
+path = "/home/mathias/PycharmProjects/Ferienakademie/test_data.npy"
+data = np.load(path).flatten()
+
+# def get_accuracy():
+#     correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
+#     accuracy = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+#     return sess.run(accuracy, feed_dict={x: mnist.test.images, y_: mnist.test.labels})
+
+for i in range(120):
+    _, loss_val = sess.run([train_step, loss], feed_dict={x: np.expand_dims(np.array([0.5]), 1), ground_truth: np.expand_dims(data, 1).transpose()})
+    print("Loss: {}".format(loss_val))
+
+data = np.reshape(data, (32, 64, 2))
+data = np.append(data, np.zeros([32, 64, 1]), axis=2)
+
+net_data = sess.run(output, feed_dict={x: np.expand_dims(np.array([0.5]), 1)})
+net_data = net_data.transpose()
+net_data = np.reshape(net_data, (32, 64, 2))
+net_data = np.append(net_data, np.zeros([32, 64, 1]), axis=2)
+
+diff = data - net_data
+diff[0,0,0] = 1
+
+misc.imsave("/home/mathias/PycharmProjects/Ferienakademie/original.png", data, "png")
+misc.imsave("/home/mathias/PycharmProjects/Ferienakademie/net.png", net_data, "png")
+misc.imsave("/home/mathias/PycharmProjects/Ferienakademie/diff.png", diff, "png")
+
+# epochs = 30
+# mini_batch_size = 10
+# mnist_train_size = 10000
+# for i in range(epochs):
+#     mini_batches = [mnist.train.next_batch(mini_batch_size) for i in range(int(mnist_train_size / mini_batch_size))]
+#     for mini_batch in mini_batches:
+#         sess.run(train_step, feed_dict={x: mini_batch[0], y_: mini_batch[1]})
+#     print("Epoch {} accuracy: {:.2f}%".format(i+1, get_accuracy() * 100))
+#
+# print("Final accuracy: {:.2f}%".format(get_accuracy() * 100))
