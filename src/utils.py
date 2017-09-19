f012c63d7b9c400875a5b814ad740b941d5a7497
Flakebi <flakebi@t-online.de>
2017-09-19 18:01:39 +0200

    Add methods for visualization

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,10 @@
 import os
 import os.path
 import math
+import scipy
+import scipy.misc
+import numpy as np
+import matplotlib.pyplot as plt
 
 import pickle
 
@@ -40,3 +44,24 @@ def deserialize(filename):
     result = pickle.load(file)
     file.close()
     return result
+
+def sim1resToImage(result, path):
+    arrToImage(result.data, path)
+
+def nn1resToImage(result, path):
+    data = result.data
+    scipy.ndimage.zoom(data, [2.0, 2.0, 1.0], order=1)
+    arrToImage(data, path)
+
+def arrToImage(data):
+    imageHeight = len(data)
+    imageWidth = len(data[0])
+
+    fig, ax = plt.subplots()
+    x, y = np.mgrid[0:imageHeight, 0:imageWidth]
+    # Every 3rd arrow
+    skip = (slice(None, None, 3), slice(None, None, 3))
+    dx, dy = np.transpose(data, (2, 0, 1))
+    ax.quiver(x[skip], y[skip], dx[skip], dy[skip])
+    ax.set(aspect=1, title='Vector field')
+    plt.show()
