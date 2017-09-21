29b636344a9902fab30c660ec7cfe333e84be8f2
annika <gannielu@gmail.com>
2017-09-21 18:28:22 +0200

    color plots

--- flow.py
+++ flow.py
@@ -14,15 +14,17 @@ def plot_flow_triple(real_flow, net_flow):
     X,Y = np.mgrid[0:image_size[0]:1, 0:image_size[1]:1]
 
     [f, (ax1, ax2, ax3)] = plt.subplots(3, sharex=True, sharey=True)
-    ax1.quiver(X,Y,real_flow[::1,::1,0], real_flow[::1,::1,1], scale=S)
+    M = np.hypot(real_flow[::1,::1,0], real_flow[::1,::1,1])
+    ax1.quiver(X,Y,real_flow[::1,::1,0], real_flow[::1,::1,1],M, scale=S)
     ax1.set_title("Real flow")
     ax1.set_xlim(0, image_size[0])
     ax1.set_ylim(0, image_size[1])
     ax2.set_title("Output of network")
-    ax2.quiver(X,Y, net_flow[::1,::1,0], net_flow[::1,::1,1], scale=S)
+    ax2.quiver(X,Y, net_flow[::1,::1,0], net_flow[::1,::1,1],M, scale=S)
 
     ax3.set_title("Plot of velocity differences (real-net)")
-    ax3.quiver(X,Y,real_flow[::1,::1,0]-net_flow[::1,::1,0],real_flow[::1,::1,1]-net_flow[::1,::1,1], scale=S)
+    N=np.hypot(real_flow[::1,::1,0]-net_flow[::1,::1,0], real_flow[::1,::1,1]-net_flow[::1,::1,1])
+    ax3.quiver(X,Y,real_flow[::1,::1,0]-net_flow[::1,::1,0],real_flow[::1,::1,1]-net_flow[::1,::1,1],N, scale=S)
 
     # compute error
     diff_flow = (real_flow[:,:,0]-net_flow[:,:,0])**2 + (real_flow[:,:,1]-net_flow[:,:,1])**2
