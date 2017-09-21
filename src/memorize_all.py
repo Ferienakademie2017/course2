3aff8f0b2a25846c8fc85418610c64464768d8c5
Mathias Jackermeier <mathias.jackermeier@tum.de>
2017-09-21 15:02:37 +0200

    updated memorize_all

--- src/memorize_all.py
+++ src/memorize_all.py
@@ -4,12 +4,12 @@ import numpy as np
 
 
 def weight_variable(shape):
-    initial = tf.truncated_normal(shape)
+    initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
 
 def bias_variable(shape):
-    initial = tf.truncated_normal(shape)
+    initial = tf.truncated_normal(shape, stddev=0.1)
     return tf.Variable(initial)
 
 
@@ -19,28 +19,41 @@ def to_image_form(data):
 
 
 def create_net(x):
-    # output layer
-    w_fc1 = weight_variable([1, 4096])
-    b_fc1 = bias_variable([4096])
-    output = tf.matmul(x, w_fc1) + b_fc1
+    hidden_layer_size = 20
+
+    # hidden layer
+    w_fc1 = weight_variable([1, hidden_layer_size])
+    b_fc1 = bias_variable([hidden_layer_size])
+    h_fc1 = tf.nn.sigmoid(tf.matmul(x, w_fc1) + b_fc1)
+
+    #output layer
+    w_fc2 = weight_variable([hidden_layer_size, 4096])
+    b_fc2 = bias_variable([4096])
+    output = tf.tanh(tf.matmul(h_fc1, w_fc2) + b_fc2)
     return output
 
 
 def create_trainer(output, ground_truth):
     loss = tf.reduce_mean(tf.reduce_sum(tf.pow(ground_truth - output, 2), reduction_indices=[1]))
-    train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
+    train_step = tf.train.AdamOptimizer(0.1).minimize(loss)
     return train_step, loss
+
 
 def load_data():
     training_image = []
     training_val = []
     for i in range(31):
-        path = "../res/karman_data/vel"+str(i+1)+".npy"
+        if i == 15: continue
+        path = "../res/karman_data_norm/vel"+str(i+1)+".npy"
         training_image.append(np.load(path).flatten())
         training_val.append((i+1)/32)
     
-    training_data = [np.reshape(training_val, (31, 1)),training_image]
+    training_data = [np.reshape(training_val, (30, 1)), training_image]
     return training_data
+
+def get_scale_factor(y_pos):
+    index = int(y_pos * 32 - 1)
+    return np.load("../res/karman_data_norm/scale_factors.npy")[index]
 
 def train():
     x = tf.placeholder(tf.float32, [None, 1])
@@ -54,30 +67,15 @@ def train():
     training_data = load_data()
 
     #train
-    for i in range(120):
-        #np.expand_dims(np.array([0.5]), 1
-        #np.expand_dims(data, 1).transpose()
+    for i in range(2000):
         _, loss_val = sess.run([train_step, loss], feed_dict={x: training_data[0], ground_truth: training_data[1]})
         print("Epoch {}: Loss = {}".format(i, loss_val))
 
-    # data = to_image_form(data)
-    # net_data = sess.run(output, feed_dict={x: np.expand_dims(np.array([0.5]), 1)})
+    net_data = sess.run(output, feed_dict={x: np.reshape([0.5], (1, 1))})
+    net_data *= get_scale_factor(0.5)
+    output_img = to_image_form(net_data)
+    np.save("../res/net_image", output_img)
 
 
 if __name__ == "__main__":
     train()
-
-    # misc.imsave("/home/mathias/PycharmProjects/Ferienakademie/original.png", data, "png")
-    # misc.imsave("/home/mathias/PycharmProjects/Ferienakademie/net.png", net_data, "png")
-    # misc.imsave("/home/mathias/PycharmProjects/Ferienakademie/diff.png", diff, "png")
-
-    # epochs = 30
-    # mini_batch_size = 10
-    # mnist_train_size = 10000
-    # for i in range(epochs):
-    #     mini_batches = [mnist.train.next_batch(mini_batch_size) for i in range(int(mnist_train_size / mini_batch_size))]
-    #     for mini_batch in mini_batches:
-    #         sess.run(train_step, feed_dict={x: mini_batch[0], y_: mini_batch[1]})
-    #     print("Epoch {} accuracy: {:.2f}%".format(i+1, get_accuracy() * 100))
-    #
-    # print("Final accuracy: {:.2f}%".format(get_accuracy() * 100))
