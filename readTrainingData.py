29a25c8c93b919d6be93e660aa4eb573c1f17f06
Jan-Nico Zaech <jan-nico@users.noreply.github.com>
2017-09-21 10:59:25 +0200

    Added function to read training data from pickle

---
+++ readTrainingData.py
@@ -0,0 +1,66 @@
+import numpy as np
+import scipy.ndimage as ndimage
+import matplotlib.pyplot as plt
+import pickle
+
+def transformToLinear(inputVectField):
+    newSize = np.prod((inputVectField.shape))
+    outputVect = np.reshape(inputVectField,newSize,order='c')
+    return outputVect
+
+def transformToImage(inputVect,fieldSize):
+    outputVectField = np.reshape(inputVect,fieldSize,order='c')
+    return outputVectField
+
+def scaleImage(inputField,scaleParam):
+    scaledField = ndimage.zoom(inputField, [1, 1/scaleParam, 1/scaleParam, 1])
+    return scaledField
+
+def showImage(inputField):
+    scaledImage = inputField - np.min(inputField)
+    scaledImage = scaledImage / (np.max(scaledImage) - np.min(scaledImage))
+    imgplot = plt.imshow(scaledImage)
+    plt.show()
+
+    def loadData(path):
+        sourceFile = open(path, "rb")
+        yPositions = pickle.load(sourceFile)
+        trainingSize = yPositions.shape[0]
+        # fieldSize = (32, 64, 2)
+        outputSize = 256
+
+        trainingInput = yPositions
+        trainingOutput = np.zeros((trainingSize, outputSize), np.float32)
+
+        for i in range(trainingSize):
+            currentOutput = pickle.load(sourceFile)
+            # showVectorField(currentOutput[0,:,:,:])
+            currentOutput = scaleImage(currentOutput, 2)
+            currentOutput = transformToLinear(currentOutput[0, :, :, 0:2])
+            trainingOutput[i, :] = currentOutput
+
+        return trainingInput,trainingOutput
+
+# def showVectorField(inputField):
+#     fig = plt.figure()
+#     M = np.hypot(inputField[:,:,0], inputField[:,:,1])
+#     [baseY,baseX] = np.meshgrid(range(inputField.shape[0]),range(inputField.shape[1]))
+#     plt.quiver(baseX, baseY, inputField[:,:,1], inputField[:,:,0], M)
+#     plt.show()
+
+if __name__ == "__main__":
+    sourceFile = open( r'C:\Users\Nico\Documents\Ferienakademie\course2\trainingData\trainingKarman1.p', "rb" )
+    yPositions = pickle.load(sourceFile)
+    trainingSize = yPositions.shape[0]
+    fieldSize = (32,64,2)
+    outputSize = 256
+
+    trainingInput = yPositions
+    trainingOutput = np.zeros((trainingSize,outputSize),np.float32)
+
+    for i in range(trainingSize):
+        currentOutput = pickle.load(sourceFile)
+        # showVectorField(currentOutput[0,:,:,:])
+        currentOutput = scaleImage(currentOutput, 2)
+        currentOutput = transformToLinear(currentOutput[0,:,:,0:2])
+        trainingOutput[i,:] = currentOutput
--- read_npy.py
+++ read_npy.py
@@ -1,12 +1,27 @@
 import numpy as np
 import scipy.misc as misc
 import matplotlib.pyplot as plt
+import pickle
+
+def transformToLinear(inputVectField):
+    newSize = np.prod((inputVectField.shape))
+    outputVect = np.reshape(inputVectField,newSize,order='c')
+    return outputVect
+
+def transformToImage(inputVect,fieldSize):
+    outputVectField = np.reshape(inputVect,fieldSize,order='c')
+    return outputVectField
 
 training_file_raw = np.load(r'C:\Users\Nico\Documents\Ferienakademie\course2\trainingData\trainingKarman1.npy')
 training_file_raw = training_file_raw.squeeze()
 training_file_plt = training_file_raw - np.min(training_file_raw)
 training_file_plt = training_file_plt/(np.max(training_file_plt)-np.min(training_file_plt))
 imgplot = plt.imshow(training_file_plt)
+
+training_file_mod = training_file_raw[:,:,0:2]
+training_file_lin = transformToLinear(training_file_mod)
+training_file_image = transformToImage(training_file_lin, (32,64,2))
+
 plt.show()
 #training_file = misc.toimage(training_file_raw)
 #training_file.show()
