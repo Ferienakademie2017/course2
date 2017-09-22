b3bb5fe7927aa7eb17ca29f9ca6bb8a3f6b8fab8
Flakebi <flakebi@t-online.de>
2017-09-22 17:33:30 +0200

    Try to adapt manual eval

--- src/manualEval.py
+++ src/manualEval.py
@@ -15,16 +15,22 @@ data = trainConfig.loadGeneratedData()
 init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
-model = models.computeNN1()
+model = models.computeNN5()
 
 # Load final variables
 model.load(sess, "final")
 
 # Sample some input data
 outputManta = data[len(data) // 2 + 1]
-manualResults = sess.run(model.yPred, evaluation.getFeedDict(model, [evaluation.ParametricSimulationExample(outputManta)]))
+outputManta.npVel = np.transpose(outputManta.npVel, (1, 0, 2))
+outputManta.obstacles = np.transpose(outputManta.obstacles)
+manualResults = sess.run(model.yPred, evaluation.getFeedDict(model, [evaluation.ParametricSimulationExample(outputManta, slice=[0, 1])]))
+
 outputTensor = Sim1Result.Sim1Result(manualResults[0], outputManta.obstacle_pos, outputManta.obstacles)
-outputTensor.npVel = np.transpose(outputTensor.npVel, (1, 0, 2))
+# outputTensor.npVel = np.transpose(outputTensor.npVel, (1, 0, 2))
+# outputTensor.obstacles = np.transpose(outputTensor.obstacles)
+# outputManta.npVel = np.transpose(outputManta.npVel, (1, 0, 2))
+# outputManta.obstacles = np.transpose(outputManta.obstacles)
 
 utils.image_i = 100
 utils.sim1resToImage(outputManta)
