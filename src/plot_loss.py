ba35ee2d10c818c18472125f7be0e8efd2dfcba8
Mathias Jackermeier <mathias.jackermeier@tum.de>
2017-09-25 10:47:59 +0200

    Finished the fully convolutional fixed time

--- src/plot_loss.py
+++ src/plot_loss.py
@@ -1,7 +1,19 @@
+# import matplotlib.pyplot as plt
+# import numpy as np
+#
+# data = np.loadtxt("../res/training_memorize_all.csv", delimiter=",")
+# plt.ylim((0,80))
+# plt.plot(*zip(*data))
+# plt.ylabel("Loss")
+# plt.xlabel("Epoch")
+# plt.show()
+
 import matplotlib.pyplot as plt
 import numpy as np
 
 data = np.loadtxt("../res/training_memorize_all.csv", delimiter=",")
+plt.figure(figsize=(10, 5))
+# plt.ylim((0,80))
 plt.plot(*zip(*data))
 plt.ylabel("Loss")
 plt.xlabel("Epoch")
