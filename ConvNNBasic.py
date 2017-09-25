453473abb01a72309a01f59aa2e863bb4c3d41d8
annika <gannielu@gmail.com>
2017-09-25 17:19:07 +0200

    new timedependent trainingData/trainingKarman_time.p ConvNNBasic.py new

--- ConvNNBasic.py
+++ ConvNNBasic.py
@@ -12,66 +12,112 @@ import flow
 # script for first basic neural net where input layer
 # (y coordinate of obstacle) is directly forwarded to output layer with 256 flow values
 
-path_to_data = r'C:\Users\Annika\Saved Games\Desktop\course2\trainingData\trainingKarman32.p'
+# path_to_data = r'C:\Users\Annika\Saved Games\Desktop\course2\trainingData\trainingKarman32.p'
+path_to_data = r'C:\Users\Annika\Saved Games\Desktop\course2\trainingData\trainingKarman32_1000randu.p'
 # path_to_data = r'C:\Users\Nico\Documents\Ferienakademie\course2\trainingData\trainingKarman32.p'
 trainingEpochs = 1000
-batchSize = 8
+batchSize = 1
 inSize = 1 # warning - hard coded to scalar values 1
+validationProportion = 0.2
 
 # set up the network
 
 x = tf.placeholder(tf.float32)
 y = tf.placeholder(tf.float32)  # training data
 
+# first layer
+size1 = 256
 xIn = tf.reshape(x, shape=[-1, inSize])  # flatten
-fc_1w = tf.Variable(tf.random_normal([inSize, 256], stddev=0.01))
-fc_1b = tf.Variable(tf.random_normal([256], stddev=0.01))
+fc_1w = tf.Variable(tf.random_normal([inSize, size1], stddev=0.01))
+fc_1b = tf.Variable(tf.random_normal([size1], stddev=0.01))
 
 fc_1 = tf.add(tf.matmul(xIn, fc_1w), fc_1b)
 fc_1= tf.nn.tanh(fc_1)
 # fc1 = tf.nn.dropout(fc1, 0.5)  # plenty of dropout...
+fc_1 = tf.reshape(fc_1, shape=[-1, 8, 16, 2])
 
-fc_2w = tf.Variable(tf.random_normal([256, 256], stddev=0.01))  # back to input size
-fc_2b = tf.Variable(tf.random_normal([256], stddev=0.01))
+# Convolutional Layer #1, 2nd overall layer
+conv1 = tf.layers.conv2d(
+    inputs=fc_1,
+    filters=2,
+    kernel_size=3,
+    padding="same",
+    activation=tf.nn.relu)
 
-fc_2 = tf.add(tf.matmul(fc_1, fc_2w), fc_2b)
+# Convolutional Layer #1, 2nd overall layer
+# conv2 = tf.layers.conv2d(
+#     inputs=conv1,
+#     filters=2,
+#     kernel_size=3,
+#     strides=(1, 2),
+#     padding="same",
+#     activation=tf.nn.relu)
+
+# Pooling Layer #1
+# pool1 = tf.layers.max_pooling2d(inputs=conv1, pool_size=[2, 2], strides=2)
+
+size2 = 256
+sizeconv1 = 256
+#pool1 = tf.reshape(pool1, shape=[1, 128])
+conv1 = tf.reshape(conv1, shape=[1, sizeconv1])
+fc_2w = tf.Variable(tf.random_normal([sizeconv1, size2], stddev=0.01))  # back to input size
+fc_2b = tf.Variable(tf.random_normal([size2], stddev=0.01))
+
+fc_2 = tf.add(tf.matmul(conv1, fc_2w), fc_2b)
 fc_2 = tf.nn.tanh(fc_2)
 # y_pred = tf.add(tf.matmul(fc1, fc_2w), fc_2b)
 # y_pred = tf.reshape(y_pred, shape=[-1, 64, 64, 1])
+# size3 = 256
+# fc_3w = tf.Variable(tf.random_normal([size2, size3], stddev=0.01))  # back to input size
+# fc_3b = tf.Variable(tf.random_normal([size3], stddev=0.01))
+#
+# fc_3 = tf.add(tf.matmul(fc_2, fc_3w), fc_3b)
+# fc_3 = tf.nn.tanh(fc_3)
+
 y_pred = fc_2
 # y_pred = tf.reshape(y_pred, shape=[-1, 8, 16, 2])
-
 cost = tf.nn.l2_loss((y - y_pred))/batchSize
 opt = tf.train.GradientDescentOptimizer(0.2).minimize(cost)
 
+# now we can start training...
 # now we can start training...
 
 # read input  and training data
 position_y, training_data = readTrainingData.loadData(path_to_data)
 # training_data = np.reshape(training_data, newshape=[-1, 8, 16, 2])
 scaling = np.ndarray.max(abs(training_data))
-training_data = training_data/scaling
+training_data = training_data / scaling
 loadNum = len(position_y)
+validationNum = round(loadNum * validationProportion)
+validationInd = random.sample(range(0, loadNum), validationNum)
+trainingInd = [ind for ind in range(0, loadNum) if ind not in validationInd]
+validationData = training_data[validationInd]
+validationInput = position_y[validationInd]
+training_data = training_data[trainingInd]
+trainingInput = position_y[trainingInd]
+trainingSize = len(training_data)
 
 print("Starting training...")
 sess = tf.InteractiveSession()
 sess.run(tf.global_variables_initializer())
 
 for epoch in range(trainingEpochs):
-    #c = (epoch * batchSize) % training_data.shape[0]
+    # c = (epoch * batchSize) % training_data.shape[0]
     batch_training_out = []
     batch_training_in = []
     for currNo in range(0, batchSize):
-        r = random.randint(0, loadNum - 1)
+        r = random.randint(0, trainingSize - 1)
         batch_training_out.append(training_data[r, :])
-        batch_training_in.append(position_y[r])
-    #batch_xs, batch_ys = training_data[c:c+batchSize,:], training_data[c:c+batchSize,:]
+        batch_training_in.append(trainingInput[r])
+    # batch_xs, batch_ys = training_data[c:c+batchSize,:], training_data[c:c+batchSize,:]
+
 
     _, currentCost = sess.run([opt, cost], feed_dict={x: batch_training_in, y: batch_training_out})
-    print("Epoch %d/%d: cost %f " % (epoch + 1, trainingEpochs, currentCost) )
+    print("Epoch %d/%d: cost %f " % (epoch + 1, trainingEpochs, currentCost))
 
     if epoch == trainingEpochs - 1:
-        [valiCost, vout] = sess.run([cost, y_pred], feed_dict={x: position_y[0], y: training_data[6,:]})
+        [valiCost, vout] = sess.run([cost, y_pred], feed_dict={x: validationInput, y: validationData})
+        valiCost = valiCost * batchSize / validationNum
         print(" Validation: cost %f " % (valiCost))
 
         if epoch == trainingEpochs - 1:
---
+++ karman_timedependent.py
@@ -0,0 +1,131 @@
+from manta import *
+import numpy as np
+import pickle
+import os
+
+#new variables
+noise = 1
+# obstacleCenter = vec3(0.25,0.75,0.5)
+t_ref = 50
+# yPositions = np.linspace(0,1,14)
+# yPositions = np.linspace(1/16,15/16,14)
+# yPositions = np.repeat(yPositions,100)
+yPositions = np.random.uniform(0,1,10)
+# yPositions = 0.5
+# print("y positions:", yPositions)
+# yPositions = np.ones([32])*0.75
+# yPositions = np.array([0.5])
+# script_dir = os.path.dirname(__file__)
+# targetFileDir = r'\trainingData\trainingKarman1.p'
+# abs_file_path = os.path.join(script_dir, targetFileDir)
+# targetFile = open( r'C:\Users\Nico\Documents\Ferienakademie\course2\trainingData\trainingKarman32.p', "wb" )
+targetFile = open( r'C:\Users\Annika\Saved Games\Desktop\course2\trainingData\trainingKarman_time.p', "wb" )
+#write params to pickle
+pickle.dump(yPositions, targetFile)
+
+secOrderBc = True
+dim        = 2
+res        = 16
+#res        = 124
+gs         = vec3(2*res,res,1)
+if (dim==2): gs.z = 1
+s          = FluidSolver(name='main', gridSize = gs, dim=dim)
+s.timestep = 1.
+
+
+flags     = s.create(FlagGrid)
+density   = s.create(RealGrid)
+vel       = s.create(MACGrid)
+density   = s.create(RealGrid)
+pressure  = s.create(RealGrid)
+fractions = s.create(MACGrid)
+phiWalls  = s.create(LevelsetGrid)
+if noise:
+    testall = s.create(RealGrid)
+    noise = s.create(NoiseField, loadFromFile=True)
+
+for p in yPositions :
+    # print('position: ', p)
+    positionslist = list()
+
+    obstacleCenter = vec3(0.25, p, 0.5)
+
+    flags.initDomain(inflow="xX", phiWalls=phiWalls, boundaryWidth=0)
+
+    #obstacle  = Sphere(   parent=s, center=gs*vec3(0.25,0.5,0.5), radius=res*0.2)
+    obstacle  = Cylinder( parent=s, center=gs*obstacleCenter, radius=res*0.2, z=gs*vec3(0, 0, 1.0))
+    phiObs    = obstacle.computeLevelset()
+
+    # slightly larger copy for density source
+    densInflow  = Cylinder( parent=s, center=gs*obstacleCenter, radius=res*0.21, z=gs*vec3(0, 0, 1.0))
+
+    phiObs.join(phiWalls)
+    updateFractions( flags=flags, phiObs=phiObs, fractions=fractions)
+    setObstacleFlags(flags=flags, phiObs=phiObs, fractions=fractions)
+    flags.fillGrid()
+
+    velInflow = vec3(0.9, 0, 0)
+    vel.setConst(velInflow)
+
+    # optionally randomize y component
+    if noise:
+        # noise = s.create(NoiseField, loadFromFile=True)
+        noise.posScale = vec3(75)
+        noise.clamp    = True
+        noise.clampNeg = -1.
+        noise.clampPos =  1.
+        # testall = s.create(RealGrid); testall.setConst(-1.)
+        testall.setConst(-1.)
+        addNoise(flags=flags, density=density, noise=noise, sdf=testall, scale=0.1 )
+
+    setComponent(target=vel, source=density, component=1)
+    density.setConst(0.)
+
+    # cg solver params
+    cgAcc    = 1e-04
+    cgIter = 5
+
+    timings = Timings()
+
+    if (GUI):
+        gui = Gui()
+        gui.show()
+        #gui.pause()
+
+    npArray = np.ones( [1,res,res*2,3], dtype=np.float32 )
+
+    #main loop
+    for t in range(t_ref):
+        mantaMsg('\nFrame %i, simulation time %f' % (s.frame, s.timeTotal))
+
+        densInflow.applyToGrid( grid=density, value=2. )
+
+        advectSemiLagrange(flags=flags, vel=vel, grid=density, order=2, orderSpace=1)
+        advectSemiLagrange(flags=flags, vel=vel, grid=vel    , order=2, strength=1.0)
+
+        if(secOrderBc):
+            extrapolateMACSimple( flags=flags, vel=vel, distance=2 , intoObs=True)
+            setWallBcs(flags=flags, vel=vel, fractions=fractions, phiObs=phiObs)
+
+            setInflowBcs(vel=vel,dir='xX',value=velInflow)
+            solvePressure( flags=flags, vel=vel, pressure=pressure, fractions=fractions, cgAccuracy=cgAcc, cgMaxIterFac=cgIter)
+
+            extrapolateMACSimple( flags=flags, vel=vel, distance=5 , intoObs=True)
+            setWallBcs(flags=flags, vel=vel, fractions=fractions, phiObs=phiObs)
+        else:
+            setWallBcs(flags=flags, vel=vel)
+            setInflowBcs(vel=vel,dir='xX',value=velInflow)
+            solvePressure( flags=flags, vel=vel, pressure=pressure, cgAccuracy=cgAcc, cgMaxIterFac=cgiter )
+            setWallBcs(flags=flags, vel=vel)
+
+        setInflowBcs(vel=vel,dir='xX',value=velInflow)
+
+        timings.display()
+        s.step()
+        inter = 10
+        copyGridToArrayVec3(target=npArray, source=vel)
+        positionslist.append(npArray)
+        if t == t_ref - 1:
+            pickle.dump(positionslist, targetFile)
+            # np.save(r'C:\Users\Nico\Documents\Ferienakademie\course2\trainingData\trainingKarman1.npy',npArray)
+
