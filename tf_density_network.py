7b9682bf923fe16c91ce6eb728215ebd006a5e0e
pylipp <philipp.metzner@tum.de>
2017-09-26 16:26:56 +0200

    Introduce validation/training indices to select data.

--- tf_density_network.py
+++ tf_density_network.py
@@ -2,7 +2,11 @@ from __future__ import division
 
 import tensorflow as tf
 import numpy as np
+import os
+import shutil
+import os.path
 
+from utils import get_parameter
 from networks import create_1_256_network, create_1_8_256_network,\
         create_1_32_deconv_256_network
 
@@ -39,29 +43,21 @@ densities = np.array(densities)
 
 print("Read fluid data samples")
 
-validationSize = int(sample_count * 0.1) # take 10% as validation samples
-validation_start_index = 14
-validation_end_index = validation_start_index + validationSize
-print("Validation data range {}:{}".format(
-    validation_start_index, validation_end_index))
-
+validation_indices = get_parameter("validation_indices")
+training_indices = get_parameter("training_indices")
 # desired output for validation and training
-validationData = densities[validation_start_index:validation_end_index][:]
-trainingData = np.vstack((
-    densities[0:validation_start_index][:],
-    densities[validation_end_index:][:]
-    ))
+validationData = densities[validation_indices]
+trainingData = densities[training_indices]
 
-# input for validation and training
-validationInput = y_positions[validation_start_index:validation_end_index]
-trainingInput = np.hstack((
-    y_positions[:validation_start_index][:],
-    y_positions[validation_end_index:][:]
-    ))
+# input for validation and training, as Xx1 vector
+validationInput = y_positions[validation_indices].reshape(-1, 1)
+trainingInput = y_positions[training_indices].reshape(-1, 1)
 
 training_batch_size = len(trainingData)
 print("Split into %d training and %d validation samples" %
         (training_batch_size, len(validationData)))
+
+print(trainingInput.shape)
 
 # set up network
 # input_layer, output = create_1_256_network(densities)
@@ -80,22 +76,27 @@ init = tf.global_variables_initializer()
 sess = tf.Session()
 sess.run(init)
 
-flat_training_data = trainingData
-trainingInput = trainingInput.reshape(-1, 1)
-print(flat_training_data.shape)
-print(trainingInput.shape)
-
 for i in range(trainingEpochs):
     if i % 100 == 99:
         print("Training epoch {}...".format(i))
     sess.run(train,
-            feed_dict={input_layer: trainingInput, y: flat_training_data})
+            feed_dict={input_layer: trainingInput, y: trainingData})
 
 # test the trained network
-# kinda hacky because the batchsize is fixed inside the network architecture, so
-# for training we give the same input batchsize times...
-# TODO: equal-sized training and validation batches
-test_output = sess.run(output,
-        {input_layer: validationInput[0] * np.ones((training_batch_size, 1))})
-formatted_test_output = test_output.reshape((training_batch_size, ) + density_shape)
-np.save("test_output", formatted_test_output[0])
+test_output = sess.run(output, {input_layer: validationInput})
+formatted_test_output = test_output.reshape(
+        (training_batch_size, ) + density_shape)
+
+script_dir = os.path.dirname(os.path.abspath(__file__))
+test_outputs_dir = os.path.join(script_dir, "testOutputs")
+if os.path.isdir(test_outputs_dir):
+    shutil.rmtree(test_outputs_dir)
+os.makedirs(test_outputs_dir)
+
+# output filename == validation index (visualization derives position of
+# obstacle on that)
+indices = list(range(densities.shape[0]))[validation_indices]
+for i in range(formatted_test_output.shape[0]):
+    np.save(os.path.join(
+        test_outputs_dir, "{:04d}".format(indices[i])),
+            formatted_test_output[i])
